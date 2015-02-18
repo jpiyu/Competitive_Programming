612ee6e3ad47091fc8d672936e88442c2ff319e2
Piyush Joshi <joshipiyush217@gmail.com>
2015-02-18 10:31:59 +0530

    done

---
+++ CppApplication_1/main.c
@@ -0,0 +1,18 @@
+/* 
+ * File:   main.c
+ * Author: Piyu
+ *
+ * Created on September 28, 2014, 2:20 PM
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+    printf("hello");
+    return (EXIT_SUCCESS);
+}
+
--- JavaApplication2/src/javaapplication2/JavaApplication2.java
+++
@@ -1,8 +0,0 @@
-package javaapplication2;
-class JavaApplication2
-{
-    public static void main(String[] args)
-    {
-        System.out.println("This is a demo program");
-    }
-}
